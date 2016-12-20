8bc84ec791e939f8e5f2ee159f19ea3deedd0a56
tomas <tomastong0108@gmail.com>
2016-12-20 21:49:09 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, :only:[:edit , :update ,:destroy]
+  before_action :find_group_and_check_permission, only:[:edit , :update ,:destroy]
   def index
     @groups = Group.all
   end
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-     
+
      @group.destroy
      flash[:alert] = "Group deleted"
      redirect_to groups_path
